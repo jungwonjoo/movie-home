9e94f4c67d57f13b447d24a0dafa0ba56d2ffb4a
jungwonjoo <wonjoo88@nate.com>
2025-05-20 19:32:47 +0900

    11

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,53 +6,51 @@ export const metadata = {
     title: "HOME"
 }
 
-
-
 async function getMovies() {
     const res = await fetch(URL)
     return res.json()
 }
 
-// export default async function Page() {
+export default async function Page() {
 
-//     const movies = await getMovies();
+    const movies = await getMovies();
 
-//     return (
-//         <div className={homeStyles.container}>
-//             {movies.map((movie) => (
-//                 <MovieItem 
-//                     key={movie.id}
-//                     id={movie.id} 
-//                     poster_path={movie.poster_path} 
-//                     title={movie.title}
-//                 />                
-//             ))}
-//         </div>
-//     )
-// }
+    return (
+        <div className={homeStyles.container}>
+            {movies.map((movie) => (
+                <MovieItem 
+                    key={movie.id}
+                    id={movie.id} 
+                    poster_path={movie.poster_path} 
+                    title={movie.title}
+                />                
+            ))}
+        </div>
+    )
+}
 
 
 // Next.js 15+에서는 page.tsx 컴포넌트가 기본적으로 { params, searchParams }를 받는다고 간주함
 // 쓰지 않더라도 타입에 명시해 줘야 함
 
-type PageProps = {
-  params: {}; // 현재 사용 안 하므로 빈 객체로 설정
-  searchParams?: {}; // optional
-};
+// type PageProps = {
+//   params: {}; // 현재 사용 안 하므로 빈 객체로 설정
+//   searchParams?: {}; // optional
+// };
 
-export default async function Page({ params }: PageProps) {
-  const movies = await getMovies();
+// export default async function Page({ params }: PageProps) {
+//   const movies = await getMovies();
 
-  return (
-    <div className={homeStyles.container}>
-      {movies.map((movie:any) => (
-        <MovieItem
-          key={movie.id}
-          id={movie.id}
-          poster_path={movie.poster_path}
-          title={movie.title}
-        />
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div className={homeStyles.container}>
+//       {movies.map((movie:any) => (
+//         <MovieItem
+//           key={movie.id}
+//           id={movie.id}
+//           poster_path={movie.poster_path}
+//           title={movie.title}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,9 +2,9 @@ import { Suspense } from 'react'
 import MovieInfo, { getMovie } from '../../../../component/movie-info'
 import MovieVideo from '../../../../component/movie-videos'
 
-// interface IParams{
-//     params:{id:string}
-// }
+interface IParams{
+    params:{id:string}
+}
 
 // export async function  generateMetadata({params:{id}}:IParams) {
     
@@ -15,29 +15,33 @@ import MovieVideo from '../../../../component/movie-videos'
 //     }
 // }
 
-type IParams = {
-  params: { id: string };
-};
 
-export async function generateMetadata({ params}: IParams ) {
-    const movie = await getMovie(params.id);
-    return {
-        title: movie.title,
-    };
+type PageProps = Promise<{ params: { id: string } }>;
+
+export async function generateMetadata(props: PageProps) {
+  const { params } = await props;
+  const movie = await getMovie(params.id);
+  return {
+    title: movie.title,
+  };
 }
 
-export default async function Page({ params}: IParams ){
 
-    const id = params.id;
+// export default async function Page({ params}: IParams ){
+
+//     const id = params.id;
+
+export default async function Page(props: PageProps) {
+  const { params } = await props;
 
     return(
         <div>  
             <Suspense fallback={<h1>기다려</h1>}>
-                <MovieInfo id={id}/>
+                <MovieInfo id={params.id}/>
             </Suspense>
 
             <Suspense fallback={<h1>기다려</h1>}>
-                <MovieVideo id={id}/>
+                <MovieVideo id={params.id}/>
             </Suspense>
         </div>
     )
