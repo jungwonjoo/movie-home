b2bafcb40cf703f82dc01ad3c42df637e930a252
jungwonjoo <wonjoo88@nate.com>
2025-05-20 18:29:27 +0900

    후 메인페이지 다시 수정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -12,20 +12,44 @@ async function getMovies() {
     return res.json()
 }
 
-export default async function Page() {
+// export default async function Page() {
 
-    const movies = await getMovies();
+//     const movies = await getMovies();
 
-    return (
-        <div className={homeStyles.container}>
-            {movies.map((movie) => (
-                <MovieItem 
-                    key={movie.id}
-                    id={movie.id} 
-                    poster_path={movie.poster_path} 
-                    title={movie.title}
-                />                
-            ))}
-        </div>
-    )
+//     return (
+//         <div className={homeStyles.container}>
+//             {movies.map((movie) => (
+//                 <MovieItem 
+//                     key={movie.id}
+//                     id={movie.id} 
+//                     poster_path={movie.poster_path} 
+//                     title={movie.title}
+//                 />                
+//             ))}
+//         </div>
+//     )
+// }
+
+
+// Next.js 15+에서는 page.tsx 컴포넌트가 기본적으로 { params, searchParams }를 받는다고 간주함
+// 쓰지 않더라도 타입에 명시해 줘야 함
+
+export default async function Page(props: {
+  params: {};
+  searchParams?: {};
+}) {
+  const movies = await getMovies();
+
+  return (
+    <div className={homeStyles.container}>
+      {movies.map((movie) => (
+        <MovieItem
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
+      ))}
+    </div>
+  );
 }
