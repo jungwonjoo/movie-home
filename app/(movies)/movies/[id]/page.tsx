8e28bcf92a541fb0d4ff6d6a3a6881e5e9a765c6
jungwonjoo <wonjoo88@nate.com>
2025-05-20 18:23:23 +0900

    promise로 수정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,20 +2,35 @@ import { Suspense } from 'react'
 import MovieInfo, { getMovie } from '../../../../component/movie-info'
 import MovieVideo from '../../../../component/movie-videos'
 
-interface IParams{
-    params:{id:string}
-}
+// interface IParams{
+//     params:{id:string}
+// }
 
-export async function  generateMetadata({params:{id}}:IParams) {
+// export async function  generateMetadata({params:{id}}:IParams) {
     
-    const movie = await getMovie(id)
+//     const movie = await getMovie(id)
     
+//     return {
+//         title: movie.title
+//     }
+// }
+
+type IParams = Promise <{id: string}>;
+
+export async function generateMetadata(props: { params: IParams }) {
+    const params = await props.params;
+    const id = params.id;
+    const movie = await getMovie(id);
     return {
-        title: movie.title
-    }
+        title: movie.title,
+    };
 }
 
-export default async function Page({params:{id}}:IParams){
+export default async function Page(props: { params: IParams }){
+
+    const params = await props.params;
+    const id = params.id;
+
     return(
         <div>  
             <Suspense fallback={<h1>기다려</h1>}>
