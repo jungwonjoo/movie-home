626fa82be909b4a30055067e94302d3c44b51f00
jungwonjoo <wonjoo88@nate.com>
2025-05-20 19:36:47 +0900

    뀨

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -28,29 +28,3 @@ export default async function Page() {
         </div>
     )
 }
-
-
-// Next.js 15+에서는 page.tsx 컴포넌트가 기본적으로 { params, searchParams }를 받는다고 간주함
-// 쓰지 않더라도 타입에 명시해 줘야 함
-
-// type PageProps = {
-//   params: {}; // 현재 사용 안 하므로 빈 객체로 설정
-//   searchParams?: {}; // optional
-// };
-
-// export default async function Page({ params }: PageProps) {
-//   const movies = await getMovies();
-
-//   return (
-//     <div className={homeStyles.container}>
-//       {movies.map((movie:any) => (
-//         <MovieItem
-//           key={movie.id}
-//           id={movie.id}
-//           poster_path={movie.poster_path}
-//           title={movie.title}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -6,42 +6,24 @@ interface IParams{
     params:{id:string}
 }
 
-// export async function  generateMetadata({params:{id}}:IParams) {
+export async function  generateMetadata({params:{id}}:IParams) {
     
-//     const movie = await getMovie(id)
+    const movie = await getMovie(id)
     
-//     return {
-//         title: movie.title
-//     }
-// }
-
-
-type PageProps = Promise<{ params: { id: string } }>;
-
-export async function generateMetadata(props: PageProps) {
-  const { params } = await props;
-  const movie = await getMovie(params.id);
-  return {
-    title: movie.title,
-  };
+    return {
+        title: movie.title
+    }
 }
 
-
-// export default async function Page({ params}: IParams ){
-
-//     const id = params.id;
-
-export default async function Page(props: PageProps) {
-  const { params } = await props;
-
+export default async function Page({params:{id}}:IParams){
     return(
         <div>  
             <Suspense fallback={<h1>기다려</h1>}>
-                <MovieInfo id={params.id}/>
+                <MovieInfo id={id}/>
             </Suspense>
 
             <Suspense fallback={<h1>기다려</h1>}>
-                <MovieVideo id={params.id}/>
+                <MovieVideo id={id}/>
             </Suspense>
         </div>
     )
